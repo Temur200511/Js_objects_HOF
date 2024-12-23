b51e895cf52062c8153528052fd93ce3981c938c
Temur200511 <tziyodullayev61@gmail.com>
2024-12-23 10:37:23 +0500

    full

--- app.js
+++ app.js
@@ -13,30 +13,40 @@
 
 
 
-// function findPrimes(numbers){
-
-// return numbers.filter(isPrime);
-
+// function isPrime(num) {
+//     if (num <= 1) return false;  // 1 yoki kichik sonlar tub emas
+//     for (let i = 2; i < num; i++) {  // Sonni 2 dan num-1 gacha bo'lgan sonlarga bo'lamiz
+//         if (num % i === 0) {
+//             return false;  // Agar bo'linadigan son topilsa, tub emas
+//         }
+//     }
+//     return true;  // Agar bo'linadigan son topilmasa, tub son
 // }
 
-// function()
+// function findPrimesFromList(numbers) {
+//     let primes = [];
+//     for (let i = 0; i < numbers.length; i++) {
+//         if (isPrime(numbers[i])) {
+//             primes.push(numbers[i]);  // Agar tub bo'lsa, ro'yxatga qo'shamiz
+//         }
+//     }
+//     return primes;
+// }
+
+// let numbers = [10, 15, 3, 7, 2, 11, 17, 18, 23];  // Berilgan raqamlar ro'yxati
+// console.log(findPrimesFromList(numbers));  // Natijani konsolga chiqaramiz
 
 
 
 
 
-// const arr = [3, 53, 6, 7]
 
 
 
 
 
 
-
-
-
-
-// function kkuchToqson(arr)
+// function kkUchToqJuftson(arr)
 // {
 //     const arr =[];
 
@@ -45,8 +55,6 @@
 //     });
  
 // }
-
-
 
 
 
@@ -65,8 +73,6 @@
 
 
 
-
-
 // 5
 
 // let matn = "54gdfffg098fc, 654, jhg"
@@ -77,4 +83,16 @@
 
 
 
+// 6
+
+// function obj(productA, productB, productC, productD ){
+//  return{
+//     productA: 4,
+//     productB: 3,
+//     productC: 2,
+//      productD: 1
+//    }
+   
+   
+// }
 
