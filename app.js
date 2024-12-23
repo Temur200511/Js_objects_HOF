3b1b39c7cd63cc59774fb6fe6b0ddcdebaf379bb
Temur200511 <tziyodullayev61@gmail.com>
2024-12-23 10:15:23 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,80 @@
+// 1
+
+
+// const str = "Men seni sevaman kitob seni sevaman";
+// let arr =str.split(" ");
+
+// const removed = arr.filter((word) => arr.indexOf(word) ===arr.lastIndexOf(word) )
+
+// console.log(removed);
+
+
+
+
+
+
+// function findPrimes(numbers){
+
+// return numbers.filter(isPrime);
+
+// }
+
+// function()
+
+
+
+
+
+// const arr = [3, 53, 6, 7]
+
+
+
+
+
+
+
+
+
+
+// function kkuchToqson(arr)
+// {
+//     const arr =[];
+
+//     arr.forEach(element => {
+        
+//     });
+ 
+// }
+
+
+
+
+
+
+
+
+
+// 4
+
+// let arr = [3, 53, 6, 7, 55, 53];
+// let sorted = arr.sort((a, b) => a-b);
+
+// console.log(sorted);
+
+ 
+
+
+
+
+
+// 5
+
+// let matn = "54gdfffg098fc, 654, jhg"
+
+// let res = matn.replace(/\d+/g, "")
+// console.log(res);
+
+
+
+
+
